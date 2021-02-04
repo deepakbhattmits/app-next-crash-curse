afa6285c802f2bd8fd071d2c6bd82f9a2b126bfb
deepak bhatt <deepakbhattmits@gmail.com>
2021-02-05 00:03:12 +0530

    Create ArticleList.jsx

---
+++ components/ArticleList.jsx
@@ -0,0 +1,16 @@
+/** @format */
+
+import ArticleItem from './ArticleItem';
+import articleStyles from '../styles/Article.module.css';
+
+const ArticleList = ({ articles }) => {
+	return (
+		<div className={articleStyles.grid}>
+			{articles.map((article) => (
+				<ArticleItem article={article} />
+			))}
+		</div>
+	);
+};
+
+export default ArticleList;
