2bbbfcd04682501c474cea219bf366bef40a959c
deepak bhatt <deepakbhattmits@gmail.com>
2021-02-05 00:03:15 +0530

    Create ArticleItem.jsx

---
+++ components/ArticleItem.jsx
@@ -0,0 +1,17 @@
+/** @format */
+
+import Link from 'next/link';
+import articleStyles from '../styles/Article.module.css';
+
+const ArticleItem = ({ article }) => {
+	return (
+		<Link href={`/article/${article.id}`}>
+			<a className={articleStyles.card}>
+				<h3>{article.title} &rarr;</h3>
+				<p>{article.body}</p>
+			</a>
+		</Link>
+	);
+};
+
+export default ArticleItem;
