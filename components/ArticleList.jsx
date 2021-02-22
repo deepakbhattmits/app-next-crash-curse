d89bedbddaff36289e5fbb0957b05ae08bedd708
deepak bhatt <deepakbhattmits@gmail.com>
2021-02-22 08:58:45 +0530

    Update ArticleList.jsx

--- components/ArticleList.jsx
+++ components/ArticleList.jsx
@@ -6,8 +6,8 @@ import articleStyles from '../styles/Article.module.css';
 const ArticleList = ({ articles }) => {
 	return (
 		<div className={articleStyles.grid}>
-			{articles.map((article) => (
-				<ArticleItem article={article} />
+			{articles.map((article, index) => (
+				<ArticleItem key={index} article={article} />
 			))}
 		</div>
 	);
