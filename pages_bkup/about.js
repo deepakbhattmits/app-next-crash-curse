340a7246e275b04b4f70feb0fbe5ffc258ce67e4
deepak bhatt <deepakbhattmits@gmail.com>
2021-04-01 09:14:05 +0530

    Create about.js

---
+++ pages_bkup/about.js
@@ -0,0 +1,12 @@
+/** @format */
+
+import Meta from '../components/Meta';
+const About = () => {
+	return (
+		<>
+			<Meta title='WebDev Newz - About page' />
+			<h1>About</h1>
+		</>
+	);
+};
+export default About;
